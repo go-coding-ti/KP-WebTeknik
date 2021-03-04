776beeba3cc57dafb2204efb4dc4fbb3419a46af
Hadi-Darmawan <ghadi.darmawan@gmail.com>
2021-03-04 23:40:09 +0800

    Perbaikan pertama navbar

--- resources/views/layouts/Navbar.blade.php
+++ resources/views/layouts/Navbar.blade.php
@@ -4,72 +4,6 @@
         <a class="nav-link link-light fw-bold link" href="#"><strong>Fakultas Teknik</strong></a>
     </div>
 </nav>
-
-
-{{-- <div class="wrapper">
-    <div class="logo"><a href="#">CodingNepal</a></div>
-    <input type="radio" name="slider" id="menu-btn">
-    <input type="radio" name="slider" id="close-btn">
-    <ul class="nav-links">
-      <label for="close-btn" class="bg-light btn close-btn"><i class="fas fa-times"></i></label>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li>
-        <a href="#" class="desktop-item">Dropdown Menu</a>
-        <input type="checkbox" id="showDrop">
-        <label for="showDrop" class="mobile-item">Dropdown Menu</label>
-        <ul class="drop-menu">
-          <li><a href="#">Drop menu 1</a></li>
-          <li><a href="#">Drop menu 2</a></li>
-          <li><a href="#">Drop menu 3</a></li>
-          <li><a href="#">Drop menu 4</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#" class="desktop-item">Mega Menu</a>
-        <input type="checkbox" id="showMega">
-        <label for="showMega" class="mobile-item">Mega Menu</label>
-        <div class="mega-box">
-          <div class="content bg-primary">
-            <div class="row">
-              <img src="img.jpg" alt="">
-            </div>
-            <div class="row">
-              <header>Design Services</header>
-              <ul class="mega-links">
-                <li><a href="#">Graphics</a></li>
-                <li><a href="#">Vectors</a></li>
-                <li><a href="#">Business cards</a></li>
-                <li><a href="#">Custom logo</a></li>
-              </ul>
-            </div>
-            <div class="row">
-              <header>Email Services</header>
-              <ul class="mega-links">
-                <li><a href="#">Personal Email</a></li>
-                <li><a href="#">Business Email</a></li>
-                <li><a href="#">Mobile Email</a></li>
-                <li><a href="#">Web Marketing</a></li>
-              </ul>
-            </div>
-            <div class="row">
-              <header>Security services</header>
-              <ul class="mega-links">
-                <li><a href="#">Site Seal</a></li>
-                <li><a href="#">VPS Hosting</a></li>
-                <li><a href="#">Privacy Seal</a></li>
-                <li><a href="#">Website design</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li><a href="#">Feedback</a></li>
-    </ul>
-    <label for="menu-btn" class="btn bg-light menu-btn"><i class="fas fa-bars"></i></label>
-</div> --}}
-
-
 <nav class="navbar sticky-top bg-red shadow-sm p-0">
     <div class="container-fluid wrapper bg-red d-flex align-content-center">
         <nav class="navbar navbar-white p-0">
@@ -77,7 +11,7 @@
                 <a class="navbar-brand p-0" href="#">
                     <img src="https://s2itp.unud.ac.id/wp-content/uploads/2018/02/logo-unud-2018.png" alt="" width="70" height="70" class="d-inline-block align-top">
                 </a>
-                <a class="navbar-brand link-light fw-bold fs-4 text-wrap" style="max-width: 250px" href="#">Fakultas Teknik Universitas Udayana</a>
+                <a class="navbar-brand link-light fw-bold fs-5 text-wrap" style="max-width: 200px" href="#">Fakultas Teknik Universitas Udayana</a>
             </div>
         </nav>
         <input type="radio" name="slider" id="menu-btn">
@@ -86,17 +20,17 @@
             <label for="close-btn" class="btn close-btn">
                 <i class="fas fa-times"></i>
             </label>
-            <li class="list-inline-item bg-info py-4">
+            <li class="list-inline-item py-4">
                 <a href="#">Home</a>
             </li>
-            <li class="list-inline-item bg-info py-4">
+            <li class="list-inline-item py-4">
                 <a href="#">About</a>
             </li>
-            <li class="list-inline-item more-links bg-info py-4">
+            <li class="list-inline-item fw-bold bg-info m-0 py-4 px-0">
                 <a href="#" class="desktop-item">Services</a>
                 <input type="checkbox" id="showMoreDrop">
-                <label class="mobile-item" for="showMoreDrop">Services</label>
-                <ul class="drop-more-menu bg-warning p-0 text-center">
+                <label for="showMoreDrop" class="mobile-item">Services</label>
+                <ul class="drop-more-menu px-0">
                     <li>
                         <a href="#">Drop Menu 1</a>
                     </li>
@@ -109,8 +43,8 @@
                     <li>
                         <a href="#" class="desktop-item">More Items</a>
                         <input type="checkbox" id="showMoreItems">
-                        <label for="showMoreItems" class="mobile-item text-center px-0">More Items</label>
-                        <ul class="drop-menu-more bg-warning p-0 text-center">
+                        <label for="showMoreItems" class="mobile-item">More Items</label>
+                        <ul class="drop-menu-more p-0 text-center">
                             <li>
                                 <a href="#">Sub Menu 1</a>
                             </li>
@@ -124,11 +58,11 @@
                     </li>
                 </ul>
             </li>
-            <li class="list-inline-item bg-info py-4">
+            <li class="list-inline-item fw-bold bg-primary m-0 px-0 py-4">
                 <a href="#" class="desktop-item">Dropdown Menu</a>
                 <input type="checkbox" id="showDrop">
                 <label for="showDrop" class="mobile-item">Dropdown Menu</label>
-                <ul class="drop-menu bg-warning p-0 text-center">
+                <ul class="drop-menu p-0">
                     <li>
                         <a href="#">Drop menu 1</a>
                     </li>
@@ -143,12 +77,12 @@
                     </li>
                 </ul>
             </li>
-            <li class="list-inline-item bg-info py-4">
+            <li class="list-inline-item fw-bold m-0 px-0 py-4">
                 <a href="#" class="desktop-item">Mega Menu</a>
                 <input type="checkbox" id="showMega">
                 <label for="showMega" class="mobile-item">Mega Menu</label>
                 <div class="mega-box">
-                    <div class="content bg-primary">
+                    <div class="content bg-red">
                         <div class="row">
                             <img src="img.jpg" alt="">
                         </div>
