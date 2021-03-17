83c741ebe3b09be69049f958011ac8aac2265330
Hadi-Darmawan <ghadi.darmawan@gmail.com>
2021-03-17 14:11:02 +0800

    Menambahkan auto slide hero pada landing page

--- resources/views/layouts/Hero.blade.php
+++ resources/views/layouts/Hero.blade.php
@@ -1,7 +1,129 @@
 <div class="container mt-5 pt-5">
   <div class="row no-gutters">
     <div class="col-12 col-md-7 col-lg-8 p-0 px-1">
-      <div class="tab-content">
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="3500" id="post-1" role="tabpanel" aria-labelledby="post-1-tab">
+            <div class="carousel-inner single-feature-post video-post bg-img">
+                <img src="https://images.unsplash.com/photo-1508921340878-ba53e1f016ec?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fEo5eXJQYUhYUlFZfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="d-block w-100" alt="...">
+                <div class="post-content">
+                  <a href="#" class="btn btn-sm btn-primary text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm btn-success text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm bg-red text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="single-post.html" class="post-title text-decoration-none link-white fw-bold">Rancangan Judul News Webiste Fakultas Teknik Universitas Udayana 1</a>
+                  <div class="post-meta d-flex">
+                    <a href="#" class="text-decoration-none">
+                      <i class="fas fa-comment" aria-hidden="true"></i>
+                      25
+                    </a>
+                    <a href="#" class="text-decoration-none">
+                      <i class="fas fa-thumbs-up" aria-hidden="true"></i>
+                      25
+                    </a>
+                  </div>
+                </div>
+                <span class="video-duration">00 Maret 1021</span>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="3500" id="post-2" role="tabpanel" aria-labelledby="post-2-tab">
+            <div class="carousel-inner single-feature-post video-post bg-img">
+              <img src="https://images.unsplash.com/photo-1480365501497-199581be0e66?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvdG9zfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="d-block w-100" alt="...">
+              <div class="post-content">
+                  <a href="#" class="btn btn-sm btn-primary text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm btn-success text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm bg-red text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                <a href="single-post.html" class="post-title text-decoration-none link-white fw-bold">Rancangan Judul News Webiste Fakultas Teknik Universitas Udayana 2</a>
+                <div class="post-meta d-flex">
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-comment" aria-hidden="true"></i>
+                    25
+                  </a>
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-thumbs-up" aria-hidden="true"></i>
+                    25
+                  </a>
+                </div>
+              </div>
+              <span class="video-duration">00 Maret 1021</span>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="3500" id="post-3" role="tabpanel" aria-labelledby="post-3-tab">
+            <div class="carousel-inner single-feature-post video-post bg-img">
+              <img src="https://images.unsplash.com/photo-1447069387593-a5de0862481e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fHBob3Rvc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="d-block w-100" alt="...">
+              <div class="post-content">
+                  <a href="#" class="btn btn-sm btn-primary text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm btn-success text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm bg-red text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                <a href="single-post.html" class="post-title text-decoration-none link-white fw-bold">Rancangan Judul News Webiste Fakultas Teknik Universitas Udayana 3</a>
+                <div class="post-meta d-flex">
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-comment" aria-hidden="true"></i>
+                    25
+                  </a>
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-thumbs-up" aria-hidden="true"></i>
+                    25
+                  </a>
+                </div>
+              </div>
+              <span class="video-duration">00 Maret 1021</span>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="3500" id="post-4" role="tabpanel" aria-labelledby="post-4-tab">
+            <div class="carousel-inner single-feature-post video-post bg-img">
+              <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="d-block w-100" alt="...">
+              <div class="post-content">
+                  <a href="#" class="btn btn-sm btn-primary text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm btn-success text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm bg-red text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                <a href="single-post.html" class="post-title text-decoration-none link-white fw-bold">Rancangan Judul News Webiste Fakultas Teknik Universitas Udayana 4</a>
+                <div class="post-meta d-flex">
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-comment" aria-hidden="true"></i>
+                    25
+                  </a>
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-thumbs-up" aria-hidden="true"></i>
+                    25
+                  </a>
+                </div>
+              </div>
+              <span class="video-duration">00 Maret 1021</span>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="3500" id="post-5" role="tabpanel" aria-labelledby="post-4-tab">
+            <div class="carousel-inner single-feature-post video-post bg-img">
+              <img src="https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8bmF0dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="d-block w-100" alt="...">
+              <div class="post-content">
+                  <a href="#" class="btn btn-sm btn-primary text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm btn-success text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                  <a href="#" class="btn btn-sm bg-red text-uppercase p-1 text-white text-decoration-none">Sports</a>
+                <a href="single-post.html" class="post-title text-decoration-none link-white fw-bold">Rancangan Judul News Webiste Fakultas Teknik Universitas Udayana 5</a>
+                <div class="post-meta d-flex">
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-comment" aria-hidden="true"></i>
+                    25
+                  </a>
+                  <a href="#" class="text-decoration-none">
+                    <i class="fas fa-thumbs-up" aria-hidden="true"></i>
+                    25
+                  </a>
+                </div>
+              </div>
+              <span class="video-duration">00 Maret 1021</span>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      {{-- <div class="tab-content">
         <div class="tab-pane fade show active" id="post-1" role="tabpanel" aria-labelledby="post-1-tab">
           <div class="carousel-inner single-feature-post video-post bg-img">
             <div class="carousel-item active">
@@ -114,7 +236,7 @@
             <span class="video-duration">00 Maret 1021</span>
           </div>
         </div>
-      </div>
+      </div> --}}
     </div>
     <div class="col-12 col-md-5 col-lg-4 p-0 px-1">
       <ul class="nav vizew-nav-tab" role="tablist">
