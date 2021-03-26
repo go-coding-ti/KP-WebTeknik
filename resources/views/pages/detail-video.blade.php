5d0f5b8421231b194992ee36f36cbf41231479de
Hadi Darmawan <ghadi.darmawan@gmail.com>
2021-03-26 09:25:53 +0800

    Menambahkan halaman video dan menambahkan multi bahasa pada bagian footer

---
+++ resources/views/pages/detail-video.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts/UserLayout')
+
+@section('content')
+  @if (App::getLocale() == 'en')
+    <div class="container pt-5">
+        <div class="card mb-3">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8" title="YouTube video" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <a href="{{ route('Video', app()->getLocale()) }}" class="btn btn-danger float-end">Return Back</a>
+            </div>
+          </div>
+    </div>
+  @endif
+
+  @if (App::getLocale() == 'id')
+    <div class="container pt-5">
+        <div class="card mb-3">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8" title="YouTube video" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <a href="{{ route('Video', app()->getLocale()) }}" class="btn btn-danger float-end">Kembali</a>
+            </div>
+          </div>
+    </div>
+  @endif
+@endsection
--- resources/views/pages/video.blade.php
+++ resources/views/pages/video.blade.php
@@ -3,31 +3,277 @@
 @section('content')
   @if (App::getLocale() == 'en')
     <div class="container pt-5">
-        <div class="card mb-3">
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
             <div class="ratio ratio-21x9 card-img-top">
-              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8" title="YouTube video" allowfullscreen></iframe>
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
             </div>
             <div class="card-body text-center">
-              <h5 class="card-title fw-bold">Mars Teknik Udayana</h5>
-              <p class="card-text lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
             </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
           </div>
+        </div>
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-0 bg-grey h-100">
+            <div class="ratio ratio-21x9 card-img-top">
+              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+            </div>
+            <div class="card-body text-center">
+              <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+              <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+            </div>
+            <div class="card-footer text-center">
+              <small class="text-muted">Diposting pada 12-Mar-2020</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul class="pagination bg-dark justify-content-center mt-5">
+        <li class="page-item">
+          <a class="page-link pagination bg-black link-light border-light" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+        </li>
+        <li class="page-item"><a class="page-link pagination bg-black link-light" href="#">1</a></li>
+        <li class="page-item"><a class="page-link pagination bg-black link-light" href="#">2</a></li>
+        <li class="page-item"><a class="page-link pagination bg-black link-light" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link pagination bg-black link-light" href="#">Next</a>
+        </li>
+      </ul>
     </div>
   @endif
 
   @if (App::getLocale() == 'id')
-    <div class="container pt-5">
-        <div class="card mb-3">
-            <div class="ratio ratio-21x9 card-img-top">
-              <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8" title="YouTube video" allowfullscreen></iframe>
-            </div>
-            <div class="card-body text-center">
-              <h5 class="card-title fw-bold">Mars Teknik Udayana</h5>
-              <p class="card-text lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
-            </div>
+  <div class="container pt-5">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
           </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card border-0 bg-grey h-100">
+          <div class="ratio ratio-21x9 card-img-top">
+            <iframe src="https://www.youtube.com/embed/ybOI_hQnNz8?controls=0" title="Judul Videonya" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>
+          </div>
+          <div class="card-body text-center">
+            <h5 class="card-title text-light fw-bold">Mars Teknik Udayana</h5>
+            <p class="card-text text-light lh-sm">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <a href="" class="card-text text-black small text-decoration-none link-danger"><i class="fab fa-youtube"></i> See on YouTube</a>
+          </div>
+          <div class="card-footer text-center">
+            <small class="text-muted">Diposting pada 12-Mar-2020</small>
+          </div>
+        </div>
+      </div>
     </div>
+    <ul class="pagination bg-dark justify-content-center mt-5">
+      <li class="page-item">
+        <a class="page-link pagination bg-black link-light border-light" href="#" tabindex="-1" aria-disabled="true">Sebelumnya</a>
+      </li>
+      <li class="page-item"><a class="page-link pagination bg-black link-light" href="#">1</a></li>
+      <li class="page-item"><a class="page-link pagination bg-black link-light" href="#">2</a></li>
+      <li class="page-item"><a class="page-link pagination bg-black link-light" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link pagination bg-black link-light" href="#">Berikutnya</a>
+      </li>
+    </ul>
+  </div>
   @endif
 @endsection
